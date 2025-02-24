c0695aa7c7fd9b8f02ad164f500b7b468c48c018
ckbuhl <ckbuhl@gmail.com>
2025-02-24 09:26:33 -0500

    feat: start of testing module

--- src/hylog/main.py
+++ src/hylog/main.py
@@ -8,21 +8,27 @@ log = logging.getLogger("hyLog")
 
 
 
-def setup_logging() -> None:
-    config_file = pathlib.Path(__file__).parent / "configs" / "stderr-json-file.json"
-    with open(config_file, "r") as f:
+def setup_logging(config_path: pathlib.Path | str) -> None:
+    config_file_path = pathlib.Path(config_path)
+    if not config_file_path.exists():
+        raise FileNotFoundError(f"Config file not found: {config_file_path}")
+
+    with open(config_file_path, "r") as f:
         config = json.load(f)
+
     logging.config.dictConfig(config)
 
-    # Setup
+    # Setup thread-safe logging.
     queue_handler = logging.getHandlerByName("queue_handler")
     if isinstance(queue_handler, logging.handlers.QueueHandler):
         queue_handler.listener.start()  # type: ignore
         atexit.register(queue_handler.listener.stop)  # type: ignore
 
 
-def main() -> None:
-    setup_logging()
+
+if __name__ == "__main__":
+    config_path = pathlib.Path(__file__).parent/ "configs" / "stderr-json-file.json"
+    setup_logging(config_path)
 
     log.debug("DEBUG message")
     log.debug("DEBUG message with extra", extra={"extra_key": "extra_value"})
@@ -36,7 +42,3 @@ def main() -> None:
 
     except ZeroDivisionError as e:
         log.exception("Exception occurred", exc_info=e)
-
-
-if __name__ == "__main__":
-    main()
