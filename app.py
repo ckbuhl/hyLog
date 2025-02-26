535f3b2b840bbe0bd135ceb380446c051438f1d3
ckbuhl <ckbuhl@gmail.com>
2025-02-26 17:11:56 -0500

    feat: added decorator functionality

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+import time
+
 from pathlib import Path
 
 from hylog import get_app_logger
 
 
-log = get_app_logger("app", Path(__file__).parent / "logs")
+log = get_app_logger("app", Path(__file__).parent / "logs", stdout_level="DEBUG")
 
-if __name__ == "__main__":
+@log.func()
+def test_func_decorator(arg1: str, *, kwarg1: int) -> str:
+    return f"arg1: {arg1}, kwarg1: {kwarg1}"
+
+@log.perf()
+def test_perf_decorator() -> None:
+    time.sleep(1)
+
+def test_logger() -> None:
     log.debug("DEBUG message")
     log.debug("DEBUG message with extra", extra={"extra_key": "extra_value"})
     log.info("INFO message")
@@ -21,3 +31,10 @@ if __name__ == "__main__":
 
     except ZeroDivisionError as e:
         log.exception("Exception occurred", exc_info=e)
+
+
+
+if __name__ == "__main__":
+    test_logger()
+    test_func_decorator("test", kwarg1=42)
+    test_perf_decorator()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,8 @@
 import pathlib
+
 import pytest
 
 from hylog import main
-
-
-
 
 
 def test_setup_logging_() -> None:
